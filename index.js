6b76a36f68b72c7da0dac94b11db6a3ac25f96ad
Anka <103377387+ann153476@users.noreply.github.com>
2023-09-28 19:24:42 +0300

    new

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+////////////////////////////////
+const accessKey = "AK8wCt8zfUTOsgY5nETTZBQvlMp5KzaV3apx7zdmcOI";
+const NumbPictHome = 18;
+const count = 18;
+////////////////////////////////
 const menuBTN = document.querySelector(".nav__menu__btn");
 const Xbtn = document.querySelector(".close__modal__btn");
 const modal = document.querySelector("#modal");
@@ -8,3 +13,54 @@ menuBTN.addEventListener("click", () => {
 Xbtn.addEventListener("click", () => {
   modal.style.transform = "translateX(100%)";
 });
+///
+
+const imagesBox = document.querySelector(".images__box");
+for (let i = 0; i < NumbPictHome; i++) {
+  imagesBox.insertAdjacentHTML("afterbegin", '<img class="img__home" />');
+}
+
+///
+const imgHome = document.querySelectorAll(".img__home");
+const topic = "people";
+const query = "tea";
+axios
+  .get(
+    `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`
+    //`https://api.unsplash.com/topics/${topic}/photos?client_id=${accessKey}&per_page=${NumbPictHome}`
+    //`https://api.unsplash.com/search/collections?client_id=${accessKey}&per_page=${NumbPictHome}&query=${query}`
+  )
+  .then((response) => {
+    const photosData = response.data;
+    console.log(photosData);
+    for (let i = 0; i < imgHome.length; i++) {
+      imgHome[i].src = photosData[i].urls.regular;
+      imgHome[i].alt = photosData[i].alt_description;
+    }
+    // for (let i = 0; i < imgHome.length; i++) {
+    //   imgHome[i].src = photosData.results[i].cover_photo.urls.regular;
+    //   imgHome[i].alt = photosData.results[i].cover_photo.alt_description;
+    // }
+    // for (let i = 0; i < imgHome.length; i++) {
+    //   imgHome[i].src = photosData[i].urls.small;
+    //   //imgHome[i].alt = photosData[i].alt_description;
+    // }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+//
+
+////////search////////////
+const myInput = document.querySelector(".input__search");
+const foundPhotoBox = document.querySelector(".found__photo__box");
+const countFound = 10; // Кількість фотографій
+function funcSearch(e) {
+  console.log(e.target.value);
+  foundPhotoBox.innerHTML = ``;
+  //const searchTerm = e.target.value;
+}
+
+myInput.addEventListener("input", funcSearch);
+
+////////////////////
